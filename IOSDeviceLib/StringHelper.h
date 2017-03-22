1624591061cd8e48fd0883a0feacbbd03db9e058
Dimitar Kerezov <dimitar.kerezov@telerik.com>
2017-03-22 14:40:25 +0200

    Fix ipa installation with spaces in path

--- IOSDeviceLib/StringHelper.h
+++ IOSDeviceLib/StringHelper.h
@@ -9,3 +9,4 @@ std::string trim_end(std::string &str);
 void split(const std::string &s, char delim, std::vector<std::string> &elems);
 std::vector<std::string> split(const std::string &s, char delim);
 void replace_all(std::string& str, const std::string& from, const std::string& to);
+std::string url_encode_without_forward_slash_and_colon(const std::string &value);
